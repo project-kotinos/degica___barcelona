52ed4b315fa62706e61967c2364ce4e20fe5e485
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-08 23:16:28 +0000

    side containers support for pg

--- yourbased.sh
+++ yourbased.sh
@@ -6,9 +6,14 @@ gem install bundler -v 2.0.1
 bundle install --jobs=3 --retry=3
 # before_script
 npm install -g barcelona
+which node
 gem install bcnd
-psql -c 'create database travis_ci_test;' -U postgres
+#psql -c 'create database travis_ci_test;' -U postgres
 # script
 set -e
+#psql -c 'drop database travis_ci_test;' 
+#psql -c 'create database travis_ci_test;'
+RAILS_ENV=test bundle exec rake db:drop
+RAILS_ENV=test bundle exec rake db:create
 bundle exec rake db:setup spec
 bcnd
