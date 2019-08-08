f7d9e36e6689831333cdd11a4cff74293ff486ad
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-08 13:31:34 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -6,7 +6,7 @@ gem install bundler -v 2.0.1
 bundle install --jobs=3 --retry=3
 # before_script
 npm install -g barcelona
-gem install bcnd --no-ri --no-rdoc
+gem install bcnd
 psql -c 'create database travis_ci_test;' -U postgres
 # script
 set -e
